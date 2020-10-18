3f9a13ea0d9e8496db5ba536c4594cd62e31ab2b
Laura-Oliveira <laura.regina.cp@gmail.com>
2020-10-17 21:04:51 -0300

    Configuração do Firebase Firestone

--- app/src/main/java/com/anota_ai/View/SplashScreenActivity.kt
+++ app/src/main/java/com/anota_ai/View/SplashScreenActivity.kt
@@ -3,28 +3,29 @@ package com.anota_ai.View
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.anota_ai.R
 import com.anota_ai.Control.LoginActivity
 
 
-class SplashScreenActivity : AppCompatActivity()
-{
+class SplashScreenActivity : AppCompatActivity() {
     /* Timer Splash Screen */
     private val SPLASH_TIME_OUT = 5000;
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Handler().postDelayed(Runnable
-        /* THis method will run right after the execution of the splash finish */
-        { /* The Main Activity will be started */
-            val intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, SPLASH_TIME_OUT.toLong()
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+
+                val intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000
         )
+
     }
 }
